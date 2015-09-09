6523ad9be7df59d68a8e995803b26b7a94e35baf
Ling Tian <sweetbellor.ling@gmail.com>
2015-09-08 21:15:38 -0400

    insert front for a doubly-linked list

--- Insert_front/src/DList1.java
+++ Insert_front/src/DList1.java
@@ -71,6 +71,16 @@ public class DList1 {
      */
     public void insertFront(int i) {
         // Your solution here.
+        DListNode1 insert_node = new DListNode1(i);
+        if (size == 0) {
+            head = insert_node;
+            tail = head;
+        } else {
+            insert_node.next = head;
+            tail.prev = insert_node;
+            head = insert_node;
+        }
+        size++;
     }
 
     /**
@@ -79,6 +89,11 @@ public class DList1 {
      */
     public void removeFront() {
         // Your solution here.
+        if (size != 0) {
+            head.next = head;
+            head.prev = null;
+            size --;
+        }
     }
 
     /**
@@ -210,4 +225,5 @@ public class DList1 {
             System.out.println("size is wrong.");
         }
     }
+
 }
